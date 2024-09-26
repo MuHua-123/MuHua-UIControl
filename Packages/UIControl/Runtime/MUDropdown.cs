5bea1cf8ff5ca582c6aaaf4736e45d0c701da1c1
MuHua-123 <136542559+MuHua-123@users.noreply.github.com>
2024-09-26 17:13:57 +0800

    更新下拉框

---
+++ Packages/UIControl/Runtime/MUDropdown.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace MuHua.UIControl {
+    public class MUDropdown : DropdownField {
+        public new class UxmlFactory : UxmlFactory<MUDropdown, UxmlTraits> { }
+        public new class UxmlTraits : DropdownField.UxmlTraits { }
+        public MUDropdown() {
+            ClearClassList();
+            AddToClassList("dropdown");
+
+            labelElement.ClearClassList();
+            labelElement.AddToClassList("unity-text-element");
+            labelElement.AddToClassList("dropdown-label");
+
+            VisualElement inputElement = this.Query<VisualElement>(null, "unity-popup-field__input");
+            inputElement.ClearClassList();
+            inputElement.AddToClassList("dropdown-box");
+
+            VisualElement textElement = this.Query<VisualElement>(null, "unity-base-popup-field__text");
+            textElement.ClearClassList();
+            textElement.AddToClassList("dropdown-text");
+
+            VisualElement arrowElement = this.Query<VisualElement>(null, "unity-base-popup-field__arrow");
+            //arrowElement.ClearClassList();
+            arrowElement.AddToClassList("dropdown-arrow");
+        }
+    }
+}
