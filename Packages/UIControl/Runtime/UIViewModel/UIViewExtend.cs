a096791e1c75afd6987423c236ec4b604944039c
MuHua-123 <136542559+MuHua-123@users.noreply.github.com>
2024-10-17 11:00:50 +0800

    扩展UI框架方法

--- Packages/UIControl/Runtime/UIViewModel/UIViewExtend.cs
+++ Packages/UIControl/Runtime/UIViewModel/UIViewExtend.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace MuHua {
     public class UIViewExtend<ViewModel> : MonoBehaviour where ViewModel : UIViewModel {
         public ViewModel view;
+        public virtual VisualElement element => view.Q<VisualElement>();
+
+        public T Q<T>(string name = null, string className = null) where T : VisualElement {
+            return element.Q<T>(name, className);
+        }
     }
 }
--- Packages/UIControl/Runtime/UIViewModel/UIViewModel.cs
+++ Packages/UIControl/Runtime/UIViewModel/UIViewModel.cs
@@ -7,5 +7,9 @@ namespace MuHua {
     public class UIViewModel : MonoBehaviour {
         public UIDocument document;
         public VisualElement element => document.rootVisualElement;
+
+        public T Q<T>(string name = null, string className = null) where T : VisualElement {
+            return element.Q<T>(name, className);
+        }
     }
 }
