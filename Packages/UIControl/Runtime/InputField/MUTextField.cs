b2469ca35fd973acc8046a323ee8672515a49865
MuHua-123 <136542559+MuHua-123@users.noreply.github.com>
2024-10-25 17:07:25 +0800

    修改输入框默认字段

--- Packages/UIControl/Runtime/InputField/MUTextField.cs
+++ Packages/UIControl/Runtime/InputField/MUTextField.cs
@@ -6,7 +6,22 @@ using UnityEngine.UIElements;
 namespace MuHua {
     public class MUTextField : TextField {
         public new class UxmlFactory : UxmlFactory<MUTextField, UxmlTraits> { }
-        public new class UxmlTraits : TextField.UxmlTraits { }
+        public new class UxmlTraits : TextField.UxmlTraits {
+            public UxmlStringAttributeDescription DefaultPrompt = new UxmlStringAttributeDescription {
+                name = "default-prompt"
+            };
+            public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc) {
+                base.Init(ve, bag, cc);
+                MUTextField textField = (MUTextField)ve;
+                textField.DefaultPrompt = DefaultPrompt.GetValueFromBag(bag, cc);
+                textField.SetDefaultPrompt();
+            }
+        }
+        public string DefaultPrompt { get; set; }
+
+        public VisualElement inputElement => this.Q<VisualElement>("unity-text-input");
+        public VisualElement textElement => inputElement.Q<VisualElement>("");
+
         public MUTextField() {
             ClearClassList();
             AddToClassList("input-field");
@@ -15,14 +30,26 @@ namespace MuHua {
             labelElement.AddToClassList("unity-text-element");
             labelElement.AddToClassList("input-field-label");
 
-            VisualElement inputElement = this.Q<VisualElement>("unity-text-input");
             inputElement.ClearClassList();
             inputElement.AddToClassList("input-field-box");
 
-            VisualElement textElement = inputElement.Q<VisualElement>("");
             textElement.ClearClassList();
             textElement.AddToClassList("unity-text-element");
             textElement.AddToClassList("input-field-text");
+
+            RegisterCallback<FocusInEvent>((evt) => { PrepareInput(); });
+            RegisterCallback<FocusOutEvent>((evt) => { SetDefaultPrompt(); });
+        }
+        public void PrepareInput() {
+            textElement.RemoveFromClassList("input-field-text-d");
+            if (text != DefaultPrompt) { return; }
+            text = "";
+        }
+        public void SetDefaultPrompt() {
+            textElement.RemoveFromClassList("input-field-text-d");
+            if (value != "") { return; }
+            text = DefaultPrompt;
+            textElement.AddToClassList("input-field-text-d");
         }
     }
 }
